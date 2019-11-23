b2c95e3ad82125884fc39a6c433041024327d7c6
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-11-23 21:10:27 +0100

    compile scss file with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 // https://www.npmjs.com/package/gulp-connect is alternative to package https://www.npmjs.com/package/gulp-webserver
+// version 4 of gulp has different syntax than version 3 - https://goede.site/setting-up-gulp-4-for-automatic-sass-compilation-and-css-injection
 
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
- 
-gulp.task('connect', function() {
+const gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  sass = require('gulp-sass');
+
+function runServer() {
   connect.server();
-});
+}
 
-// gulp.task('default', ['connect']); - syntax for gulp 3
-// gulp version from 4 uses different way of defining tasks
-// https://codeburst.io/switching-to-gulp-4-0-271ae63530c0
-gulp.task('default', gulp.series('connect'));
+function compileScss() {
+  return (
+    gulp
+      .src('scss/*.scss')
+      .pipe(sass())
+      .on('error', sass.logError)
+      .pipe(gulp.dest('css'))
+  )
+}
+
+function watchFiles() {
+  gulp.watch('scss/*.scss', compileScss);
+}
+
+const build = gulp.parallel(runServer, watchFiles);
+
+exports.default = build;
